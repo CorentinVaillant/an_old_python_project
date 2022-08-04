51fe8c146d86159cbe331048b80c479884247b05
Corentin VAILLANT <c.vaillant848@gmail.com>
2022-08-04 21:29:03 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -5,42 +5,82 @@ import pyperclip as pc
 
 
 def crypt():
-    pc.copy(cryptage.cryptLong(UncryptInputText.get(),cryptage.cle))
+    cryptInputText.delete("1.0", "end-1c")
+    cryptInputText.insert(INSERT,list(cryptage.cryptLong(UncryptInputText.get("1.0", "end-1c"),cryptage.cle)))
+
 def unCrypt():
-    pc.copy(cryptage.unCryptLong(cryptInputText.get(),cryptage.cle))
+    UncryptInputText.delete("1.0", "end-1c")
+    UncryptInputText.insert(INSERT,cryptage.unCryptLong(stringToList(cryptInputText.get("1.0", "end-1c")),cryptage.cle))
+
+def copyCrypt():
+    pc.copy(cryptInputText.get("1.0", "end-1c"))
+def copyUncrypt():
+    pc.copy(str(UncryptInputText.get("1.0", "end-1c")))
+
+def stringToList(mot):
+
+    mot = mot.split('} {')
+
+    b = []
+    for k in mot:
+        b.append(k.replace('{',''))
+    mot = []
+    for k in b:
+        mot.append(k.replace('}',''))
+    for k in range(len(mot)):
+        if mot[k] == '':
+            del mot[k]
+    b = []
+    for k in range(len(mot)):
+        mot[k] = mot[k].split(' ')
+        for i in range(len(mot[k])):
+            mot[k][i] = int(mot[k][i])
+    return mot
 
 gui = Tk()
 gui.title("crypt App")
-gui.geometry('250x200')
+gui.geometry('520x250')
+
+
+gui.grid_columnconfigure(0, weight=1)
+gui.grid_columnconfigure(1, weight=1)
 
 #crypt:
 
 #label
 label=Label(gui,text="Enter your sentence to crypt")
 
-label.pack()
+label.grid(column=0,row=0, padx=0, pady=0)
 
 #input
-UncryptInputText = Entry(gui,bd=1)
-UncryptInputText.pack()
+UncryptInputText = Text(gui,bd=1,width=22,height=10)
+UncryptInputText.grid(column=0,row=1, padx=0, pady=0)
+
+#copy button
+copyButton = Button(gui, text="copy", bd='2', command=copyUncrypt)
+copyButton.grid(column=0,row=2, padx=0, pady=0)
 
 #button
-submit1 = Button(gui, text="copy result", bd='2', command=crypt)
-submit1.pack()
+submit1 = Button(gui, text="→", bd='2', command=crypt)
+submit1.place(x=250,y=75)
 
 #uncrypt
 
 #label
 label=Label(gui,bd='2',text="Enter your sentence to Uncrypt")
 
-label.pack()
+label.grid(column=1,row=0, padx=0, pady=0)
 
 #input
-cryptInputText = Entry(gui,bd=1)
-cryptInputText.pack()
+cryptInputText = Text(gui,bd=1,width=22,height=10)
+cryptInputText.grid(column=1,row=1, padx=0, pady=0)
+
+#copy button
+copyButton2 = Button(gui, text="copy", bd='2', command=copyCrypt)
+copyButton2.grid(column=1,row=2, padx=0, pady=0)
 
 #button
-submit2 = Button(gui, text="copy result", bd='2', command=unCrypt)
-submit2.pack()
+submit2 = Button(gui, text="←", bd='2', command=unCrypt)
+submit2.place(x=250,y=100)
 
 gui.mainloop()
--- cryptage.py
+++ cryptage.py
@@ -1,8 +1,9 @@
-#constantes:
+# constantes:
 import base64
 
-cle = [17,42,254] #[a(compris entre 2 et 20), b , c]   !!cle n'est pas une constante mais pratique pour l'exercice !!
-BASE="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+cle = [17, 5,5]  # [a(compris entre 2 et 20), b , c]   !!cle n'est pas une constante mais pratique pour l'exercice !!
+BASE = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 
 def help():
     a = ('''
@@ -21,91 +22,109 @@ def help():
     /!\your keycode must be a list of thre integer, you can use a default one with the variable 'cle'/!\ 
     ''')
     print(a)
-    return(a)
+    return (a)
 
-def crypt(mot, cle) :
+
+def crypt(mot, cle):
     lettres = list(mot)
-    lettres = [format(ord(i), '08b') for i in lettres]  #convertion binnaire
+    lettres = [format(ord(i), '08b') for i in lettres]  # convertion binnaire
 
     for l in range(len(lettres)):
         # inversion 0 et 1
         lettres[l] = lettres[l].replace('0', 'o')
-        lettres[l] = lettres[l].replace('1','0')
-        lettres[l] = lettres[l].replace('o','1')
+        lettres[l] = lettres[l].replace('1', '0')
+        lettres[l] = lettres[l].replace('o', '1')
 
-        lettres[l] = ''.join(reversed(lettres[l]))  #changement sens
+        lettres[l] = ''.join(reversed(lettres[l]))  # changement sens
 
-        lettres[l] = int(lettres[l],2)  #conversion en int
+        lettres[l] = int(lettres[l], 2)  # conversion en int
 
-        lettres[l] = lettres[l] + cle[1] + cle[2]*(cle[0]**l)    #ajout de la clée2 (plus clée 3 à chaque lettre)
+        lettres[l] = lettres[l] + cle[1] + cle[2] * (cle[0] ** l)  # ajout de la clée2 (plus clée 3 à chaque lettre)
 
-        lettres[l] = baseN(lettres[l],cle[0]+l) #changement base selon clé 1
+        lettres[l] = baseN(lettres[l], cle[0] + l)  # changement base selon clé 1
 
-        lettres[l] = int(lettres[l],36)                    #convertion en base 36
+        lettres[l] = int(lettres[l], 36)  # convertion en base 36
 
-    return(lettres)
-
+    return (lettres)
 
 
 def unCrypt(lettres, clee):
     for l in range(len(lettres)):
+        lettres[l] = baseN(lettres[l], 36)
 
-        lettres[l] = baseN(lettres[l],36)
+        lettres[l] = int(lettres[l], clee[0] + l)  # changement base selon clé 1
 
-        lettres[l] = int(lettres[l],clee[0]+l)  #changement base selon clé 1
+        lettres[l] = lettres[l] - cle[1] - cle[2] * (
+                    cle[0] ** l)  # soustraction de la clée2 (plus clée 3 à chaque lettre)
 
-        lettres[l] = lettres[l] - cle[1] - cle[2]*(cle[0]**l)   #soustraction de la clée2 (plus clée 3 à chaque lettre)
+        lettres[l] = '{0:08b}'.format(lettres[l])  # convertion bin
 
-        lettres[l] = '{0:08b}'.format(lettres[l])   #convertion bin
+        lettres[l] = ''.join(reversed(lettres[l]))  # changement sens
 
-        lettres[l] = ''.join(reversed(lettres[l]))  #changement sens
-
-    #inversion 0 et 1
+        # inversion 0 et 1
         lettres[l] = str(lettres[l])
         lettres[l] = lettres[l].replace('1', 'o')
-        lettres[l] = lettres[l].replace('0','1')
-        lettres[l] = lettres[l].replace('o','0')
-        lettres[l] = int(lettres[l],2)
+        lettres[l] = lettres[l].replace('0', '1')
+        lettres[l] = lettres[l].replace('o', '0')
+        lettres[l] = int(lettres[l], 2)
 
-        lettres[l] = chr(lettres[l])    #convertion char
+        lettres[l] = chr(lettres[l])  # convertion char
 
-    return(lettres)
+    return ''.join(lettres)
 
 
 def baseN(n, b):
     reste = ""
     while n:
-        reste+=BASE[n%b]
-        n//= b
+        #print(n)
+        reste += BASE[n % b]
+        n //= b
     return reste[::-1] or "0"
 
+
 def motALaBonneTaille(phrase):
-    if type(phrase) == str: li = False
-    elif type(phrase) == list: li = True
-    else : print('error, not a list or a string !'); quit()
+    if type(phrase) == str:
+        li = False
+    elif type(phrase) == list:
+        li = True
+    else:
+        print('error, not a list or a string !'); quit()
     mots = []
     phrase = list(phrase)
-    while len(phrase)> 20:
-        if li :mots.append((phrase[:21]))
-        elif li != True : mots.append(''.join(phrase[:21]))
-        del phrase[:21]
+    while len(phrase) > 10:
+        if li:
+            mots.append((phrase[:11]))
+        elif not li:
+            mots.append(''.join(phrase[:11]))
+        del phrase[:11]
 
-    if li: mots.append((phrase[:21]))
-    elif li != True:mots.append(''.join(phrase[:21]))
-    return (mots)
+    if li:
+        mots.append((phrase[:11]))
+    elif not li:
+        mots.append(''.join(phrase[:11]))
+    return mots
 
-def cryptLong(mot,cle):
+
+def cryptLong(mot, cle):
     phrase = []
     mot = motALaBonneTaille(mot)
     for k in mot:
-        phrase.append(crypt(k,cle))
-    return (phrase)
-def unCryptLong(lettres,cle):
-    #lettres = motALaBonneTaille(lettres)
-    phrase = []
-    for k in lettres:
-        phrase.append((unCrypt(k, cle)))
-        print(unCrypt(k, cle))
-    return (''.join(phrase))
+        phrase.append(crypt(k, cle))
+    return phrase
 
-print(cryptLong('test il s agit du mot exprimant la focntion d une expérience ayant pour but d experimenter ',cle))
+
+def unCryptLong(mot, cle):
+    phrase = []
+    for k in mot:
+        phrase.append(unCrypt(k,cle))
+    return ''.join(phrase)
+
+
+#test
+def test():
+    print("crypt('test',cle):",crypt('test',cle))
+    print("unCrypt(crypt('test',cle),cle):",unCrypt(crypt('test',cle),cle))
+
+    print (cryptLong('test il s agit du mot exprimant la focntion d une expérience ayant pour but d experimenter ', cle))
+    print(unCryptLong(cryptLong("test il s'agit du mot exprimant la focntion d'une expérience ayant pour but d'experimenter", cle),cle))
+    print('all the functions works !!')
